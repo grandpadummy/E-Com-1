4c6ea5094d56b2c2aa4ad63024c97e2db2ee5205
clear <grandpadummy911@gmail.com>
2026-02-12 02:30:18 -0600

    crap1

--- main/build/settings.py
+++ main/build/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'crossover.proxy.rlwy.net',
         'PORT': '16899',
     }
